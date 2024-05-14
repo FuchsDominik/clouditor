a09343bcc7d54d7fecd59c2a98247cfa45f8e629
Fuchs, Dominik <dominik.fuchs@adesso-service.com>
2024-05-14 16:33:49 +0200

    add report extraction

--- service/discovery/gvm/gvm.go
+++ service/discovery/gvm/gvm.go
@@ -33,8 +33,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-
-	"github.com/fsnotify/fsnotify"
+	"time"
 
 	"clouditor.io/clouditor/v2/api/discovery"
 	"clouditor.io/clouditor/v2/api/ontology"
@@ -81,28 +80,12 @@ func (d *gvmDiscovery) List() (list []ontology.IsResource, err error) {
 
 	// Create connection to GVM container
 	return d.discoverOperatingSystem()
-
-	//TODO: Check for errors
-	// Forward the results to the evaluation module
 }
 
 func (d *gvmDiscovery) discoverOperatingSystem() (providers []ontology.IsResource, err error) {
 
-	// Define the command and parameters
-	// cmd := exec.Command("bash", "-c", "ls -la") // Works!
-
-	// A buffer to capture the output
-	// var out bytes.Buffer
-	// cmd.Stdout = &out
-
-	// Run the command
-	// errorMessage := cmd.Run()
-	// if errorMessage != nil {
-	//	return nil, errorMessage
-	// }
-
 	os := &ontology.OperatingSystem{
-		Name:            "Linux",
+		Name:            "Ubuntu",
 		Vulnerabilities: []*ontology.Vulnerability{},
 	}
 
@@ -114,52 +97,52 @@ func (d *gvmDiscovery) discoverOperatingSystem() (providers []ontology.IsResourc
 	// We use the KEV catalog to check if the vulnerability has been exploited in the past
 	data, err := loadFileAsString("/Users/dominik.fuchs/Documents/clouditor/internal/kevcatalog/kevcatalog.json") //TODO: Update catalog regurarly to get updated CVEs
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		fmt.Println("Error reading kev catalog:", err)
 	}
 
-	fmt.Println(len(data))
+	fmt.Println("kev catalog length: ", len(data))
 
 	// Map the xml structure to ontology structure
 	for _, result := range results {
-		for _, ref := range result.NVT.Refs.Ref { //TODO: Here, we create a new vulnerability for each CVE, maybe we should create one vulnerability with multiple CVEs
-			vul := &ontology.Vulnerability{}
-			vul.Id = result.ID
-			vul.Name = result.Name
-			vul.Port = result.Port
-			vul.Severity = float32(result.Severity) // Convert int to float32
-			vul.Threat = result.Threat
-			vul.Family = result.NVT.Family
-			// Ref specific
-			vul.Cve = ref.ID
+		vul := &ontology.Vulnerability{}
+		vul.Id = result.ID
+		vul.Name = result.Name
+		vul.Port = result.Port
+		vul.Severity = float32(result.Severity) // Convert int to float32
+		vul.Threat = result.Threat
+		vul.Family = result.NVT.Family
+		// Cve specific
+		vul.Exploitable = false
+		// vul.Cve = [] // TODO: Add CVEs as array
+
+		for _, ref := range result.NVT.Refs {
+			// vul.Cve = ref.ID
 
 			// Check if the vulnerability has been exploited in the past
-			if data != "" {
-				vul.Exploitable = strings.Contains(data, vul.Cve) // Maybe check for "CVE-2021-1234" instead of CVE-2021-1234 to make sure we catch the whole CVE
-			} else {
-				vul.Exploitable = false
+			if data != "" && strings.Contains(data, ref.ID) { // Maybe check for "CVE-2021-1234" instead of CVE-2021-1234 to make sure we catch the whole CVE
+				vul.Exploitable = true
 			}
-
-			os.Vulnerabilities = append(os.Vulnerabilities, vul)
 		}
+		fmt.Println("Vulnerability name in the end: ", vul.Name)
+		fmt.Println("And the exploitability is: ", vul.Exploitable)
+		os.Vulnerabilities = append(os.Vulnerabilities, vul)
 	}
 
 	fmt.Println("Vulnerabilities found: ", len(os.Vulnerabilities))
-	// fmt.Println("First Vulnerability: ", os.Vulnerabilities[0].Name)  -> jQuery < 1.6.3 XSS Vulnerability
 
 	if err != nil {
 		log.Fatal("Error while evaluating vulnerabilities:", err)
 	}
 
-	// Print the output from the command
-	// fmt.Println("Command Output:", out.String())
+	for i := 0; i < len(os.Vulnerabilities); i++ {
+		fmt.Println("Operating System's vulnerability " + fmt.Sprint(i+1) + ": ")
+		fmt.Printf("%v\n", os.Vulnerabilities[i]) // TODO: Does not show the exploitability
+	}
 
 	return []ontology.IsResource{os}, err
 }
 
 func (d *gvmDiscovery) collectEvidences() (results []Result, err error) {
-
-	// Define the command and parameters
-	// cmd := exec.Command("docker", "exec", "greenbone-community-container-gvmd-1", "python3", "/home/GreenbonePythonScript/authenticatedScript.py", filename)
 
 	targetId, err := getTargetId()
 
@@ -199,11 +182,28 @@ func (d *gvmDiscovery) collectEvidences() (results []Result, err error) {
 	}
 	fmt.Println("Started task, report Id: ", reportId)
 
-	// Check for new results of the scan
-	// reportText, err := d.watchResults(directory, filename) // filename
+	reportFormatId, err := getReportFormatId()
 	if err != nil {
-		fmt.Printf("Error while watching the results: %v", err)
+		fmt.Printf("Error while getting the report format: %v", err)
 		return
+	}
+	fmt.Println("reportFormatId: ", reportFormatId)
+
+	// Regularly check for the results
+	reportChan := make(chan GetReportsResponse)
+	errChan := make(chan error)
+
+	go monitorScan(reportChan, errChan, taskId, reportId, reportFormatId)
+
+	for {
+		select {
+		case report := <-reportChan:
+			fmt.Println("Scan finished!")
+			return report.Report.Results, nil
+		case err = <-errChan:
+			fmt.Println("Error while monitoring the scan: ", err)
+			return
+		}
 	}
 
 	// Go on to parse the report
@@ -238,65 +238,6 @@ func (d *gvmDiscovery) collectEvidences() (results []Result, err error) {
 		// fmt.Println("First CVE: ", results[0].NVT.Refs.Ref[0].ID) // -> Throws error, since 1. result does not have a CVE reference
 	*/
 	return
-}
-
-// WatchResults creates a watcher for the file and returns the content of the file
-func (d *gvmDiscovery) watchResults(directory string, filename string) (contentText []byte, err error) {
-
-	// Create a channel to receive the file content
-	contentCh := make(chan []byte)
-
-	// Set up a new file watcher
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	// Start listening for events
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					log.Println("Error in the event the file created:", err)
-					return
-				}
-				fmt.Println("Event:", event)
-				if event.Op&fsnotify.Create == fsnotify.Create {
-					if event.Name == directory+"/"+filename {
-						fmt.Println("File Created:", event.Name)
-						content, err := os.ReadFile(event.Name)
-						contentText = content
-						if err != nil {
-							log.Println("Error reading file:", err)
-							continue
-						}
-						// Send the file content to the channel
-						contentCh <- content
-						return
-					}
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					log.Println("error:", err)
-					return
-				}
-				log.Println("error:", err)
-			}
-		}
-	}()
-
-	err = watcher.Add(directory)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Receive the file content from the channel
-	contentText = <-contentCh
-
-	return contentText, nil
-
 }
 
 // loadFileAsString reads the entire file content as a single string
@@ -427,4 +368,126 @@ func startTask(taskId string) (string, error) {
 		return "", nil
 	}
 	return response.ReportID, nil
+}
+
+func getReportFormatId() (string, error) {
+	cmd := exec.Command("bash", "-c", `ssh -i ~/.ssh/gvm kali@192.168.178.112 -f 'gvm-cli --gmp-username admin --gmp-password ff4e1015-ccdf-476d-baad-13bb657f552e socket --xml "<get_report_formats/>"'`)
+
+	// Execute the command and collect the output
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Could not run report format command: ", err)
+	}
+
+	var response GetReportFormatsResponse
+
+	err = xml.Unmarshal(out, &response)
+	if err != nil {
+		fmt.Println("Error unmarshaling report format XML:", err)
+		return "", err
+	}
+
+	// Loop through all report formats and find the one with the specified name
+	for _, format := range response.ReportFormats {
+		if format.Name == "Anonymous XML" {
+			fmt.Printf("ID of 'Anonymous XML' report format: %s\n", format.ID)
+			return format.ID, nil
+		}
+	}
+
+	return "", nil
+}
+
+// monitorScan checks the scan status periodically and processes the report when done.
+func monitorScan(reportChan chan<- GetReportsResponse, errChan chan<- error, taskId string, reportId string, reportFormatId string) {
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			cmd := exec.Command("bash", "-c", `ssh -i ~/.ssh/gvm kali@192.168.178.112 -f 'gvm-cli --gmp-username admin --gmp-password ff4e1015-ccdf-476d-baad-13bb657f552e socket --xml "<get_tasks/>"'`)
+
+			// Execute the command and collect the output
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				fmt.Println("Could not run get tasks command: ", err)
+			}
+
+			var response GetTasksResponse
+
+			err = xml.Unmarshal(out, &response)
+			if err != nil {
+				fmt.Println("Error unmarshaling get tasks XML:", err)
+				// return "", err
+				errChan <- err
+			}
+
+			// Loop through all configs and find the one with the specified name
+			taskFound := false
+			finished := false
+			for _, task := range response.Tasks {
+				if task.ID == taskId {
+					taskFound = true
+					if task.Status == "Done" {
+						fmt.Println("Task finished!")
+						finished = true
+					}
+				}
+			}
+			if !taskFound {
+				fmt.Println("Could not find task. Are you sure it's running?")
+				// return "", err
+			}
+
+			if taskFound && finished {
+
+				// Simulate report processing
+				cmdSecond := exec.Command("bash", "-c", fmt.Sprintf(`ssh -i ~/.ssh/gvm kali@192.168.178.112 -f 'gvm-cli --gmp-username admin --gmp-password ff4e1015-ccdf-476d-baad-13bb657f552e socket --xml "<get_reports report_id=\"%s\" filter=\"apply_overrides=0 levels=hmlgf min_qod=70 first=1 sort-reverse=severity ignore_pagination=1\" details=\"1\" format_id=\"%s\"/>"'`, reportId, reportFormatId))
+
+				// Execute the command and collect the output
+				out, err = cmdSecond.CombinedOutput()
+				if err != nil {
+					fmt.Println("Could not run get report command: ", err)
+					return
+				}
+
+				var response GetReportsResponse
+
+				err = xml.Unmarshal(out, &response)
+				if err != nil {
+					fmt.Println("Error unmarshaling get report XML:", err)
+					errChan <- err
+					return
+				}
+
+				results := response.Report.Results
+				fmt.Println("Unmarshalling worked! Here is the length of the results: ", len(results))
+				// Seems ok, Authenticated Scan / LSC Info Consolidation (Linux/Unix SSH Login)
+				// fmt.Println("First Name: ", results[0].Name) // Ok, Operating System (OS) End of Life (EOL) Detection
+				// fmt.Println("results: ", len(results))
+				count := 0
+
+				// Only keep the CVE references
+				for i := range response.Report.Results {
+					var cves []Ref
+					// fmt.Println("Refs: ", len(results[i].NVT.Refs.Ref))
+					if len(response.Report.Results[i].NVT.Refs) != 0 {
+						for _, ref := range response.Report.Results[i].NVT.Refs {
+							if ref.Type == "cve" {
+								cves = append(cves, ref)
+								count++
+							}
+						}
+						response.Report.Results[i].NVT.Refs = cves
+					}
+				}
+				fmt.Println("CVE count:", count)
+				reportChan <- response
+				return
+			} else {
+				fmt.Println("Scan not finished...")
+			}
+		}
+	}
 }
--- service/discovery/gvm/results_types.go
+++ service/discovery/gvm/results_types.go
@@ -1,6 +1,8 @@
 package gvm
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+)
 
 // These types are used by Unmarshal to parse the xml to structs.
 
@@ -10,7 +12,7 @@ type GetTargetsResponse struct {
 	Targets Target   `xml:"target"`
 }
 
-type Target struct {
+type Target struct { //TODO: Change to list of Tasks
 	ID    string `xml:"id,attr"`
 	Hosts string `xml:"hosts"`
 }
@@ -43,18 +45,41 @@ type StartTaskResponse struct {
 	ReportID string   `xml:"report_id"`
 }
 
-// The ultimate report
+// Manage the reponse of the get tasks request
+
+type GetTasksResponse struct {
+	XMLName xml.Name `xml:"get_tasks_response"`
+	Tasks   []Task   `xml:"task"`
+}
+
+type Task struct {
+	ID     string `xml:"id,attr"`
+	Name   string `xml:"name"`
+	Status string `xml:"status"`
+}
+
+// Manage the response of the report format repuest
+
+type GetReportFormatsResponse struct {
+	XMLName       xml.Name       `xml:"get_report_formats_response"`
+	ReportFormats []ReportFormat `xml:"report_format"`
+}
+
+// ReportFormat reflects each report format entry in the XML
+type ReportFormat struct {
+	ID   string `xml:"id,attr"`
+	Name string `xml:"name"`
+}
+
+// The final report
+
+type GetReportsResponse struct {
+	XMLName xml.Name `xml:"get_reports_response"`
+	Report  Report   `xml:"report>report"`
+}
 
 type Report struct {
-	Report ReportData `xml:"report"`
-}
-
-type ReportData struct {
-	Results Results `xml:"results"`
-}
-
-type Results struct {
-	Result []Result `xml:"result"`
+	Results []Result `xml:"results>result"`
 }
 
 type Result struct {
@@ -70,7 +95,40 @@ type Result struct {
 type NVT struct {
 	OID    string `xml:"oid,attr"`
 	Family string `xml:"family"`
-	Refs   Refs   `xml:"refs"`
+	Refs   []Ref  `xml:"refs>ref"`
+}
+
+type Ref struct {
+	Type string `xml:"type,attr"`
+	ID   string `xml:"id,attr"`
+}
+
+/*type Report struct {
+	Report ReportData `xml:"report"`
+}
+
+type ReportData struct {
+	Results Results `xml:"results"`
+}
+
+type Results struct {
+	ResultList []Result `xml:"result"`
+}
+
+type Result struct {
+	XMLName  xml.Name `xml:"result"`
+	ID       string   `xml:"id,attr"`
+	Name     string   `xml:"name"`
+	Port     string   `xml:"port"`
+	NVT      NVT      `xml:"nvt"`
+	Threat   string   `xml:"threat"`
+	Severity float32  `xml:"severity"`
+}
+
+type NVT struct {
+	OID    string `xml:"oid,attr"`
+	Family string `xml:"family"`
+	Refs   Refs   `xml:"refs"` // Maybe also no Refs
 }
 
 type Refs struct {
@@ -81,7 +139,7 @@ type Refs struct {
 type Ref struct {
 	Type string `xml:"type,attr"`
 	ID   string `xml:"id,attr"`
-}
+}*/
 
 /* Example Refs:
 <ref type="url" id="https://bugs.jquery.com/ticket/11290"/>
